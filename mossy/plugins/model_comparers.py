81c2304af64216fed12db22e84e010ed4b477c34
jotomicron <jotomicron@gmail.com>
2015-11-12 20:52:57 +0000

    Changed model_comparers to conform to actual nomenclature
    Also added a comparer that takes only one inner comparer and uses it to
    all domains.

--- mossy/plugins/model_comparers.py
+++ mossy/plugins/model_comparers.py
@@ -2,10 +2,10 @@ from mossy import utils
 from mossy.parse_config import plugin
 
 @plugin()
-class from_list_comparer:
+class integrative_comparer:
     """
     Constructor:
-        from_list_comparer(inner)
+        integrative_comparer(inner)
     where
         `ìnner` is a comparer that can compare one sequence of concepts with
             another sequence of concepts
@@ -15,10 +15,10 @@ class from_list_comparer:
     where
         `one` and `two` are models
     
-    A model is a dictionary that associates a set of ontology names with
-    annotations that are concpets extracted from those ontologies. A model is
-    converted to a set by aggregating all the concept in the same set,
-    irrespective of the ontology they come from.
+    A model is a dictionary that associates a set of domain names with
+    annotations that are concpets extracted from those domains. A model is
+    converted to a list of annotations by combining together the concept in one
+    set, irrespective of the domain they come from.
     """
     
     def __init__(self, inner):
@@ -36,7 +36,7 @@ class simple_model_comparer:
     Constructor:
         simple_model_comparer(inners, aggr)
     where
-        `ìnners` is a dictionary that associates ontology names with list
+        `ìnners` is a dictionary that associates domain names with list
             comparers
         `aggr` is an object that contains the .aggregate method. Common values
             include the plugins `model_min`, `model_max` and `model_avg`.
@@ -46,12 +46,12 @@ class simple_model_comparer:
     where
         `one` and `two` are models
     
-    A model is a dictionary that associates a set of ontology names with
-    annotations that are concpets extracted from those ontologies. This comparer
-    compares all concepts from the model `one` and ontology `A` with all
-    concepts from the model `two` and ontology `A`, for every òntology `A` and
-    constructs a dictionary that associates each ontology name with a value.
-    Then comparer then aggregates this dictionary using the `aggr` object.
+    A model is a dictionary that associates a set of domain names with
+    annotations that are concpets extracted from those domains. This comparer
+    compares all concepts from the model `one` and domain `A` with all
+    concepts from the model `two` and domain `A`, for every òntology `A` and
+    constructs a dictionary that associates each domain name with a value.
+    The comparer then aggregates this dictionary using the `aggr` object.
     
     See also: `model_min`, `model_max`, `model_avg`
     """
@@ -80,13 +80,59 @@ class simple_model_comparer:
 
 
 @plugin()
+class aggregative_comparer:
+    """
+    Constructor:
+        aggregative_comparer(inner, aggr)
+    where
+        `ìnner` is a comparer to compare cocnepts within each domain
+        `aggr` is an object that contains the .aggregate method. Common values
+            include the plugins `model_min`, `model_max` and `model_avg`.
+    
+    Usage:
+        .compare(one, two)
+    where
+        `one` and `two` are models
+    
+    A model is a dictionary that associates a set of domain names with a list
+    of annotations (concepts from those domains). This comparer compares all
+    concepts from the model `one` and domain `A` with all concepts from the
+    model `two` and domain `A`, for every domain `A` and constructs a dictionary
+    that associates each domain name with a value. Then, the comparer aggregates
+    this dictionary using the `aggr` object.
+    
+    See also: `model_min`, `model_max`, `model_avg`
+    """
+    
+    def __init__(self, inner, aggr):
+        self.inner = inner
+        self.aggr = aggr
+    
+    
+    def compare(self, one, two):
+        similarities = {}
+        
+        # Get the list of domains to consider (the intersection of them)
+        domains = set(one).intersect(two)
+        if not domains:
+            return 0
+        
+        for domain in domains:
+            first = one[domain]
+            second = two[domain]
+            similarities[domain] = self.inner.compare(first, second)
+        
+        return self.aggr.aggregate(similarities, one, two)
+
+
+@plugin()
 class model_min:
     """
     Constructor:
         model_min()
     
     This object is used by the simple_model_comparer to aggregate the various
-    similarity values obtained for each ontology and returns the minimum of all
+    similarity values obtained for each domain and returns the minimum of all
     those similarity values
     """
     
@@ -101,7 +147,7 @@ class model_max:
         model_max()
     
     This object is used by the simple_model_comparer to aggregate the various
-    similarity values obtained for each ontology and returns the maximum of all
+    similarity values obtained for each domain and returns the maximum of all
     those similarity values
     """
     
@@ -115,17 +161,17 @@ class model_avg:
     Constructor:
         model_min(weights=None)
     where
-        `weights` is a dictionary of the weight of each ontology
+        `weights` is a dictionary of the weight of each domain
     
     This object is used by the simple_model_comparer to aggregate the various
-    similarity values obtained for each ontology and returns the average of
-    those similarity values. You can specify weight values for each ontology
-    as a dictioanry that associates ontology names with their weights. The
+    similarity values obtained for each domain and returns the average of
+    those similarity values. You can specify weight values for each domain
+    as a dictioanry that associates domain names with their weights. The
     weights can be integers or float values.
     
     Alteratively, weights can be set to the string "proportional", in which
-    the weight of each ontology is set as the number of distinct annotations
-    for that ontology in both models.
+    the weight of each domain is set as the number of distinct annotations
+    for that domain in both models.
     """
     
     def __init__(self, weights=None):
